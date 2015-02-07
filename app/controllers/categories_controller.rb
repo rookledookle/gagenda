0f1059c71e1477ca0e582aec814f9aea293b3d89
Natalie Loh <Natalie@natalies-mbp.home>
2015-02-07 16:06:39 +0000

    added CRUD methods for category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,30 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.all
   end
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.create(category_params)
   end
 
   def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update 
+    @category = Category.update(category_params)
   end
 
   def show
+    @category = Category.find(params[:id])
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
