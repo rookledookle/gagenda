405ba6eb35d611e3f7cc319b7ea8fc5bedd9c4f4
Natalie Loh <Natalie@natalies-mbp.home>
2015-02-07 16:10:44 +0000

    added CRUD methods for classroom controller

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,13 +1,36 @@
 class ClassroomsController < ApplicationController
   def index
+    @classrooms = Classroom.all
   end
 
   def new
+    @classroom = Classroom.new
+  end
+
+  def create
+    @classoom = Classroom.create(classroom_params)
+    if @classroom.save
+      redirect_to classrooms_path
+    end
   end
 
   def edit
+    @classroom = Classroom.find(params[:id])
+  end
+
+  def update
+    @classroom = Classroom.update(classroom_params)
+    if @classroom.save
+      redirect_to classroom_path(@classroon)
+    end
   end
 
   def show
+    @classroom = Classroom.find(params[:id])
+  end
+
+  private
+  def classroom_params
+    params.require(:classroom).permit(:name, :description, :chair_rating)
   end
 end
