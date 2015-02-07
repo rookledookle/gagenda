1511b60abcf2e00b23ef766bc17f7e4b7293ae70
Natalie Loh <Natalie@natalies-mbp.home>
2015-02-07 16:15:40 +0000

    added CRUD methods for course model

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,13 +1,38 @@
 class CoursesController < ApplicationController
   def index
+    @courses = Course.all
   end
 
   def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.create(course_params)
+    if @course.save
+      redirect_to courses_path
+    else
+      redirect_to new_course_path
+    end
   end
 
   def edit
+    @course = Course.find(params[:id])
+  end
+
+  def update
+    @course = Course.update(course_params)
+    if @course.save
+      redirect_to course_path(@course)
+    end
   end
 
   def show
+    @course = Course.find(params[:id])
+  end
+
+  private
+  def course_params
+    params.require(:course).permit(:name, :duration, :start_date, :end_date, :price)
   end
 end
