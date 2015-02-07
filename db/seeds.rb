3acbc19b407cf9488d3669e124a1f27ce036cdf2
Natalie Loh <Natalie@natalies-mbp.home>
2015-02-07 16:42:27 +0000

    ran a rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,15 @@ Category.delete_all
 Course.delete_all
 Classroom.delete_all
 
-# CATEGORY SEED DATA #
-c1 = Category.create(name:"Full Stack")
-c2 = Category.create(name:"Front-End")
-c3 = Category.create(name:"Back-End")
-c4 = Category.create(name:"Design")
-c5 = Category.create(name:"Short Courses")
-c6 = Category.create(name:"Workshops")
-c7 = Category.create(name:"Immersive Courses")
+# CATEGORIES SEED DATA #
+ca1 = Category.create(name:"Full Stack")
+ca2 = Category.create(name:"Front-End")
+ca3 = Category.create(name:"Back-End")
+ca4 = Category.create(name:"Design")
+ca5 = Category.create(name:"Short Courses")
+ca6 = Category.create(name:"Workshops")
+ca7 = Category.create(name:"Immersive Courses")
+
+# COURSES SEED DATA #
+
+co1= Course.create(name:"Web Development Immersive", duration:"12 Weeks", start_date:"7 Jan 2015", end_date: start_date + 3.months, price:8000)
