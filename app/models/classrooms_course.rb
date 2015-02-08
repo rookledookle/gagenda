55250b2b20802297185b0c51f5c28a826cbb156f
Natalie Loh <Natalie@Natalies-MacBook-Pro.local>
2015-02-08 19:56:01 +0000

    changed nav bar position and links, made category index page

--- app/models/classrooms_course.rb
+++ app/models/classrooms_course.rb
@@ -2,3 +2,5 @@ class ClassroomsCourse < ActiveRecord::Base
   belongs_to :classroom
   belongs_to :course 
 end
+
+# created a join table to allow a course to be assigned to more than one classroom. enable functionality to instructors to book classrooms for different lesson purposes.
