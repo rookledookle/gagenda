8b78337fe4fec39a019214f4b836d003c2b43037
Natalie Loh <Natalie@Natalies-MacBook-Pro.local>
2015-02-09 12:17:42 +0000

    added minimal form helper and library for simple form gem and changed course form to reflect that style

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'foundation-rails'
+gem 'simple_form'
 
 
 gem 'jquery-rails'
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_and_belongs_to_many :classrooms
+  has_and_belongs_to_many :categories
 
   validates_presence_of :name, :price, :duration, :start_date, :end_date, :message => "Cannot be blank!"
   validates :price, exclusion: { in: %w(£ ¢ $ ), message: "%{value} is reserved." }, numericality: { greater_than: 0 }
