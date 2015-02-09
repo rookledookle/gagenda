f7e09e3f88504851563cb18991e73f4e43e89f62
Natalie Loh <Natalie@Natalies-MBP.home>
2015-02-09 23:24:54 +0000

    added image column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150209165613) do
+ActiveRecord::Schema.define(version: 20150209232432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20150209165613) do
     t.integer  "role_id"
     t.string   "name"
     t.integer  "course_id"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
