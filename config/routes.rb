eee87f8ad2bc3e69093035317402eb152f210c5d
Natalie Loh <Natalie@Natalies-MacBook-Pro.local>
2015-02-10 16:55:28 +0000

    installed rails admin mounted as admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
  
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   get '/users/:id', to: 'users#show', as: :user_profile
   resources :classrooms
