3c47fb88358cdc19b4baa10ad38c5c3cbc7c11f5
Natalie Loh <Natalie@Natalies-MacBook-Pro.local>
2015-02-10 17:46:00 +0000

    deleted moderator role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
-protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:sign_up) << :user_image
-    devise_parameter_sanitizer.for(:sign_up) << :remote_user_image_url
-    devise_parameter_sanitizer.for(:account_update) << :name
-    devise_parameter_sanitizer.for(:account_update) << :user_image
-    devise_parameter_sanitizer.for(:account_update) << :remote_user_image_url
-  end
+  protected
+   def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << :name
+      devise_parameter_sanitizer.for(:sign_up) << :user_image
+      devise_parameter_sanitizer.for(:sign_up) << :remote_user_image_url
+      devise_parameter_sanitizer.for(:account_update) << :name
+      devise_parameter_sanitizer.for(:account_update) << :user_image
+      devise_parameter_sanitizer.for(:account_update) << :remote_user_image_url
+    end
 end
