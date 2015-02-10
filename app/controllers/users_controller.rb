1f9bd684a02273052e9dc817eec228d5cdf0f2b6
Natalie Loh <Natalie@Natalies-MacBook-Pro.local>
2015-02-10 17:36:53 +0000

    tweaked separate nav bar for admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,36 +4,36 @@ class UsersController < ApplicationController
     @user = User.find(current_user[:id])
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  # def edit
+  #   @user = User.find(params[:id])
+  # end
 
-  def update
-    @user = User.find(params[:id])
-    @user.update!(user_params)
-    if @user.save
-      redirect_to user_path(@user)
-      flash[:notice] = "You successfully altered a human being!"
-    end
-  end
+  # def update
+  #   @user = User.find(params[:id])
+  #   @user.update!(user_params)
+  #   if @user.save
+  #     redirect_to user_path(@user)
+  #     flash[:notice] = "You successfully altered a human being!"
+  #   end
+  # end
 
   def show
     @user = User.find(current_user[:id])
   end
 
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    redirect_to courses_path
-    flash[:notice] = "You successfully destroyed a user."
-  end
+  # def destroy
+  #   @user = User.find(params[:id])
+  #   @user.destroy
+  #   redirect_to courses_path
+  #   flash[:notice] = "You successfully destroyed a user."
+  # end
 
-  def new
-    @user = User.find(current_user[:id])
-  end
+  # def new
+  #   @user = User.find(current_user[:id])
+  # end
 
-  private
-  def user_params
-    params.require(:user).permit(:name, :email, :role_id, :created_at, :course_id, :user_image, :remote_user_image_url)
-  end
+  # private
+  # def user_params
+  #   params.require(:user).permit(:name, :email, :role_id, :created_at, :course_id, :user_image, :remote_user_image_url)
+  # end
 end
