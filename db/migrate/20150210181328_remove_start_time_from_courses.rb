5b9b51530998458b6fb24d2d357c8e3e8ab4e5ab
Natalie Loh <Natalie@Natalies-MacBook-Pro.local>
2015-02-11 10:46:34 +0000

    removed irrelevant migration files

--- db/migrate/20150210181328_remove_start_time_from_courses.rb
+++
@@ -1,5 +0,0 @@
-class RemoveStartTimeFromCourses < ActiveRecord::Migration
-  def change
-    remove_column :courses, :start_time, :time
-  end
-end
--- db/migrate/20150210181344_remove_end_time_from_courses.rb
+++
@@ -1,5 +0,0 @@
-class RemoveEndTimeFromCourses < ActiveRecord::Migration
-  def change
-    remove_column :courses, :end_time, :time
-  end
-end
