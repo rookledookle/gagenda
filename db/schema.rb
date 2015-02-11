854d5a2ce84cfa47121793daeec60f5180223781
Natalie Loh <Natalie@Natalies-MacBook-Pro.local>
2015-02-11 14:24:01 +0000

    added bio column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211140459) do
+ActiveRecord::Schema.define(version: 20150211142330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,7 +88,7 @@ ActiveRecord::Schema.define(version: 20150211140459) do
     t.string   "name"
     t.integer  "course_id"
     t.string   "user_image"
-    t.text     "bio"
+    t.string   "biography"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
