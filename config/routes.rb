e949d410f07317bc3482bec640150d34db4be7f1
Natalie Loh <Natalie@Natalies-MacBook-Pro.local>
2015-02-11 20:06:59 +0000

    css stuuf

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
- 
+  devise_scope :user do
+    root 'devise/registrations#new'
+  end
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   get '/users/:id', to: 'users#show', as: :user_profile
-  get '/users', to: 'users#index', as: :instructor_profiles
+  get '/instructors', to: 'users#index', as: :instructor_profiles
   resources :classrooms
   resources :categories
   resources :courses do
@@ -14,8 +16,6 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root 'courses#index'
 
   # Example of regular route:
   # get 'products/:id' => 'catalog#view'
